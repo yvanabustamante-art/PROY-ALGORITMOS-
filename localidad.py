8989f5d8d5ef27294e84b3dae2cebb1754afc437
Yvana Bustamante <yvanabustamante-art@users.noreply.github.com>
2026-08-09 13:15:54 -0400

    Update localidad.py

--- localidad.py
+++ localidad.py
@@ -1,4 +1,15 @@
-class Ubicacion:
-    """
-    Clase padre que almacena las coordenadas geograficas (latitud y longitud) de cualquier punto en el mapa.
-    """
+class Ubicacion: 
+
+    def __init__(self, latitud, longitud): 
+        self.latitud = latitud 
+        self.longitud = longitud
+
+class Localidad(Ubicacion):
+    
+    def __init__(self, municipio, localidad, latitud, longitud): 
+        super().__init__(latitud, longitud) 
+        self.municipio = municipio 
+        self.localidad = localidad
+
+    def mostrar_datos(self): 
+        print("Municipio: " + self.municipio + " | Localidad: " + self.localidad + " | Latitud: " + str(self.latitud) + " | Longitud: " + str(self.longitud))
